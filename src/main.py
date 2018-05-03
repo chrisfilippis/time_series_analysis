c3e3e35dff952c63bebc8c37497fc8f43bb7b0c9
Chris Filippis <filippis.chris@outlook.com>
2018-05-03 16:30:08 +0300

    finalize double exponential smoothing

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import os.path
-import pandas
+import pandas as pd
 import matplotlib.pyplot as plt
-from sklearn.svm import SVR
+from sklearn.metrics import mean_squared_error
+import numpy as np
 
 def parse_file(input_file_path, output_file_path):
     
@@ -25,30 +26,72 @@ def parse_file(input_file_path, output_file_path):
 
     input_file.close()
 
-parse_file('C:\Users\cfilip09\Downloads\dblp.xml\dblp.xml', 'C:\Master\TimeSeries\data\\result.csv')
+def format_data(intermediate_file_path, last_year=2017):
+    year_df = pd.read_csv(intermediate_file_path, names=["year"])
+    select2017 = year_df['year'] <= last_year
 
+    all_data_df = year_df.groupby('year')['year'].count().reset_index(name="count")
+    filterd_data_df = year_df[select2017].groupby('year')['year'].count().reset_index(name="count")
+    filterd_data_df['year'] = filterd_data_df['year'].apply(lambda x: int(x))
+    filterd_data_df['count'] = filterd_data_df['count'].apply(lambda x: float(x))
+    return all_data_df, filterd_data_df
 
-year_df = pandas.read_csv('C:\Master\TimeSeries\data\\result.csv', names=["year"])
-select2016 = year_df['year'] <= 2016
-all_data = year_df.groupby('year')['year'].count().reset_index(name="count").as_matrix()
-data = year_df[select2016].groupby('year')['year'].count().reset_index(name="count").as_matrix()
+def double_exponential_smoothing_extended(series, alpha, beta, n_pred):
+    result = [series[0]]
+    level_result = []
+    trend_result = []
+    forecasts = []
 
-year_data = list()
+    for n in range(1, len(series) + (n_pred - 1)):
+        if n == 1:
+            level, trend = series[0], series[1] - series[0]
 
-for item in data:
-    year_data.append([item[0]])
+        if n >= len(series): # we are forecasting
+          value = result[-1]
+        else:
+          value = series[n]
 
-# print data[:, 0]
-# print data[:, 1]
+        last_level, level = level, alpha*value + (1-alpha)*(level+trend)
+        trend = beta*(level-last_level) + (1-beta)*trend
 
-svr_rbf = SVR(kernel='rbf', C=1e3, gamma=0.1)
-svr_poly = SVR(kernel='poly', C=1e3, degree=2)
-y_rbf = svr_rbf.fit(year_data, data[:,1]).predict(year_data)
-#y_poly = svr_poly.fit(year_data, data[:,1]).predict(year_data)
+        level_result.append(level)
+        trend_result.append(trend)
+        result.append(level+trend)
 
-plt.plot(year_data, data[:,1])
-plt.plot(year_data, y_rbf, color='navy', lw=2, label='RBF model')
-#plt.plot(year_data, y_poly, color='darkorange', lw=2, label='Polynomial model')
+    return result
+
+def mse(series, alpha):
+    result = [series[0]] # first value is same as series
+    for n in range(1, len(series)):
+        result.append(alpha * series[n] + (1 - alpha) * result[n-1])
+        
+    return result
+
+input_file = 'C:\Users\cfilip09\Downloads\dblp.xml\dblp.xml'
+intermediate_file = 'C:\Projects\Master\\time_series_analysis\data\\result.csv'
+
+last_year = 2015
+n_predictions = 2
+alpha = 0.9
+beta = 0.9
+
+parse_file(input_file, intermediate_file)
+all_data_df, filterd_data_df = format_data(intermediate_file, last_year)
+
+years = filterd_data_df['year'].as_matrix()
+init_series = filterd_data_df['count'].as_matrix()
+
+actual_series = init_series
+
+pred_years = years
+for x in range(1, n_predictions):
+    pred_years = np.append(pred_years, last_year + x)
+
+result = double_exponential_smoothing_extended(actual_series, alpha, beta, n_predictions)
+
+plt.plot(all_data_df['year'].as_matrix(), all_data_df['count'].as_matrix(), color='green', lw=2, label='actual')
+plt.plot(years, actual_series, color='navy', lw=2, label='prediction')
+plt.plot(pred_years, result, color='red', lw=2)
 plt.ylabel('number of articles')
 plt.xlabel('year')
 plt.title('Time series regression')
